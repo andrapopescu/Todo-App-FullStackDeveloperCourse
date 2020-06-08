1bc5c88ff65c0ec300c96c23916478daf10411ca
andrapopescu <andrapop2007@gmail.com>
2020-06-08 17:42:47 +0100

    Add ability to tick off a todo item

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import sys
 
-from flask import Flask, abort, jsonify, render_template, request
+from flask import Flask, abort, jsonify, redirect, render_template, request, url_for
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
@@ -17,12 +17,12 @@ class Todo(db.Model):
     completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
-        return f'<Todo {self.id} {self.description}>'
+        return f'<Todo {self.id} {self.description} (completed: {self.completed})>'
 
 
 @app.route('/')
 def index():
-    data = Todo.query.all()
+    data = Todo.query.order_by('id').all()
     return render_template('index.html', data=data)
 
 
@@ -46,3 +46,21 @@ def create_todo_item():
         abort(400)
     else:
         return jsonify(body)
+
+
+@app.route('/todos/<todo_id>/set-completed/', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json().get('completed')
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
