52388293dce2e5d716988ec0c9e6c4d0cfab50b4
andrapopescu <andrapop2007@gmail.com>
2020-06-08 16:24:10 +0100

    Add migrations functionality

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 import sys
 
 from flask import Flask, abort, jsonify, render_template, request
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:postgres@localhost:5432/todoapp'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
-
-
-db.create_all()
 
 
 @app.route('/')
